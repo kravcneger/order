7736f8c59275c6bb4dbbfaba53f714fb93fc59ac
Ivan kravtsov <kravcneger@gmail.com>
2023-09-23 00:32:31 +0600

    Add dish and ingredient models and relation model

---
+++ app/models/dish.rb
@@ -0,0 +1,5 @@
+class Dish < ApplicationRecord
+	validates :name, length: { minimum: 2, maximum: 255 }, uniqueness: true
+  has_many :dish_ingredients
+  has_many :ingredients, through: :dish_ingredients
+end
