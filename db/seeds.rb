d692670bb62c4476c6be14670a6f2f45828e65f2
Ivan kravtsov <kravcneger@gmail.com>
2023-09-24 00:22:35 +0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+[[Dish,'dishes'],
+[Ingredient,'ingredients'],
+[Composition,'compositions'],[Subscriber,'subscribers'],[Exclusion,'exclusions']].each do |entity|
+  entity[0].delete_all
+  file = Rails.root.join('db', "#{entity[1]}.yml")
+  config = YAML::load_file(file)
+  entity[0].create!(config)
+end
