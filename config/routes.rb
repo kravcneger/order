624eb842827256549af692d89969bfa0612cc13b
Ivan kravtsov <kravcneger@gmail.com>
2023-09-25 00:46:19 +0600

    Add Dishes controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 	root 'subscribers#index'
   post 'subscribers/sign_in'
   get 'subscribers/index'
-	get 'orders/index'
+	resources :orders, only: [:index]
+	resources :dishes, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
